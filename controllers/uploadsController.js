bc50e8001dbbed0fa3a8bdb5c215509604f81932
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-23 14:25:13 -0500

    Image verification

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,22 @@
 const path = require('path');
 const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
 
 exports.uploadProductImage = async(req, res) => {
+    if(!req.files){
+        throw new CustomError.BadRequestError('No file uploaded');
+    }
     const productImage = req.files.image;
+
+    if(!productImage.mimetype.startsWith('image')){
+        throw new CustomError.BadRequestError('Please Upload Image');
+    };
+
+    const maxSize = 1000;
+
+    if(productImage.size > maxSize){
+        throw new CustomError.BadRequestError('Please upload a smaller image')
+    };
 
     const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
 
